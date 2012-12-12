d699f8bba13ac3d15049a76d1a79985bfe279bee
Ruth Baylis <ruthroygbiv@yahoo-co-uk>
2012-12-12 15:49:43 +0000

    Deleted some test...

--- test/spambot/WebPageImplTest.java
+++ test/spambot/WebPageImplTest.java
@@ -2,13 +2,12 @@ package spambot;
 
 import org.junit.*;
 import static org.junit.Assert.*;
-import spambot.WebPageImpl;
+//import spambot.WebPageImpl;
 
 public class WebPageImplTest {
 	@Test
 	public void testsItAll() throws Exception {
 		WebPageImpl wp = new WebPageImpl("www.google.com");
-                wp.getContents();
 	}
 
 }
